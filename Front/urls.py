87cecc22979a1d7e33212566f253af7318551a2b
Jaheim007 <jaheimkouaho@gmail.com>
2022-08-23 16:48:17 +0000

    done with all pages

--- Front/urls.py
+++ Front/urls.py
@@ -1,8 +1,13 @@
-from Front.views import home , about 
+from Front.views import home , about , services , departments , doctor , contact, appointment
 from django.urls import path
 
 urlpatterns = [
-    path('' , home , name='home') , 
-    path('about', about , name='about')
+    path('' , home , name='home'), 
+    path('about', about , name='about'),
+    path('services', services , name='services'),
+    path('contact', contact , name='contact'),
+    path('department', departments , name='department'),
+    path('doctor', doctor , name='doctor'),
+    path('appointment' , appointment, name= "appointment") 
 ]
  
--- Front/views.py
+++ Front/views.py
@@ -5,3 +5,20 @@ def home(request):
 
 def about(request):    
     return render(request , "pages/about.html" , locals())
+
+def services(request):   
+    return render(request , "pages/services.html" , locals())
+
+def departments(request):    
+    return render(request , 'pages/departments.html' , locals())
+
+def appointment(request):    
+    return render(request , 'pages/appointments.html' , locals())
+
+def contact(request):    
+    return render(request , 'pages/contact.html' , locals())
+
+def doctor(request):    
+    return render(request , 'pages/doctors.html' , locals())
+
+
